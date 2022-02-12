f2d64703ea13554ef09e0a074ae982e64fda3d96
chadderz <walzmr@g.cofc.edu>
2022-02-11 23:50:11 -0500

    completed hw4

--- assignments/hw4/hw4.py
+++ assignments/hw4/hw4.py
@@ -1,14 +1,13 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: <Margaux Walz>
+lab4.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: This program uses graphics to create a greeting card
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
+
 
 from graphics import *
 
@@ -24,11 +23,11 @@ def squares():
 
     # create a space to instruct user
     inst_pt = Point(width / 2, height - 10)
-    instructions = Text(inst_pt, "Click to move circle")
+    instructions = Text(inst_pt, "Click to move square")
     instructions.draw(win)
 
     # builds a circle
-    shape = Circle(Point(50, 50), 20)
+    shape = Rectangle(Point(70, 70), Point(20, 20))
     shape.setOutline("red")
     shape.setFill("red")
     shape.draw(win)
@@ -42,22 +41,54 @@ def squares():
         # point where the user clicked
         change_x = click.getX() - center.getX()
         change_y = click.getY() - center.getY()
+        cloned = shape.clone()
+        cloned.draw(win)
         shape.move(change_x, change_y)
 
+    clickclose = Text(Point(200, 200), "Click anywhere to close")
+    clickclose.draw(win)
     win.getMouse()
     win.close()
 
 
 def rectangle():
-    pass
+    win = GraphWin("Rectangle", 400, 400)
+    instructions = Text(Point(200, 300), "click two opposite corner points to make a rectangle!")
+    instructions.draw(win)
+    y1 = 0
+    x1 = 0
+    y2 = 0
+    x2 = 0
+    num_clicks = 2
+    for i in range(1, 3):
+        click1 = win.getMouse()
+        y1 = click1.getY()
+        x1 = click1.getX()
+        print(x1 + y1)
+
+    rectanglepts = rectangle()
 
 
 def circle():
-    pass
+    win = GraphWin("Rectangle", 400, 400)
+    instructions = Text(Point(200, 300), "click twice to make a circle")
+    instructions.draw(win)
+
 
 
 def pi2():
-    pass
+    terms = eval(input("How many terms to sum?"))
+    sum1 = 1
+    frac = 0
+    num = 4
+    den = 1
+    for i in range(0, terms+1):
+        frac = 2 * (i % 2) - 1
+        num *= -1
+        sum1 = sum1 + frac
+    sum1 = sum1 * 4
+
+    print("pi approximation: " + sum1)
 
 
 if __name__ == '__main__':
